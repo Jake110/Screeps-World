3367fd92f08481ea0cb060fbdd4e43db40dbe87a
Jake Joules <jake2105@gmail.com>
2026-02-12 12:03:54 +0000

    logging

--- main.js
+++ main.js
@@ -101,11 +101,14 @@ module.exports.loop = function () {
 	_source = harvest.pick(spawn);
 	if (_source) {
 		// Build roads between source and Spawn/Controller
+		console.log("Start: [" + _source.pos.x + ", " + _source.pos.y + "]");
 		for (let target in [spawn, _source.room.controller]) {
+			console.log("\tEnd: [" + target.pos.x + ", " + target.pos.y + "]");
 			for (let step in _source.pos.findPathTo(target, {
 				ignoreCreeps: true,
 				swampCost: 1,
 			})) {
+				console.log("\t\tStep: [" + step.x + ", " + step.y + "]");
 				_source.room.createConstructionSite(
 					step.x,
 					step.y,
