d6c1cb1fa7a57ba0ecb1182b400ab2401985f976
Jake Joules <Jake2105@gmail.com>
2026-02-23 22:08:01 +0000

    allow towers to heal creeps, but keep a reserve energy level

--- structure.tower.js
+++ structure.tower.js
@@ -5,28 +5,46 @@ module.exports = {
 		room.find(FIND_MY_STRUCTURES, {
 			filter: { structureType: STRUCTURE_TOWER },
 		}).forEach(function (tower) {
+			let energy = tower.store[RESOURCE_ENERGY];
+			let capacity = tower.store.getCapacity();
 			[10, 20, 50].forEach(function (range) {
 				let hostile = combat.ranged_target(tower.pos, range);
 				if (hostile) {
 					tower.attack(hostile);
 					return null;
 				}
-				let damaged_structure = tower.pos.findClosestByRange(
-					FIND_MY_STRUCTURES,
+				if (energy > capacity / 4) {
+					let damaged_structure = tower.pos.findClosestByRange(
+						FIND_MY_STRUCTURES,
+						{
+							filter: function (structure) {
+								if (tower.inRangeTo(structure, range)) {
+									return false;
+								}
+								return structure.hits < structure.hitsMax;
+							},
+						},
+					);
+					if (damaged_structure) {
+						tower.repair(damaged_structure);
+						return null;
+					}
+				}
+			});
+			if (energy > capacity / 3) {
+				let damaged_creep = tower.pos.findClosestByRange(
+					FIND_MY_CREEPS,
 					{
-						filter: function (structure) {
-							if (tower.inRangeTo(structure, range)) {
-								return false;
-							}
-							return structure.hits < structure.hitsMax;
+						filter: function (creep) {
+							return creep.hits < creep.hitsMax;
 						},
 					},
 				);
-				if (damaged_structure) {
-					tower.repair(damaged_structure);
+				if (damaged_creep) {
+					tower.heal(damaged_creep);
 					return null;
 				}
-			});
+			}
 		});
 	},
 };
