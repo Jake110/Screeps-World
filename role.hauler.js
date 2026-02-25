2af513559d7fc601cbf982fe44bb49bfd69818be
Jake Joules <jake2105@gmail.com>
2026-02-25 12:56:47 +0000

    fixing method name

--- role.hauler.js
+++ role.hauler.js
@@ -2,7 +2,7 @@ const hauler = require("creep.hauler");
 
 module.exports = {
 	run: function (creep) {
-		hauler.check_capacity(creep, RESOURCE_ENERGY);
+		hauler.capacity_check(creep, RESOURCE_ENERGY);
 		if (creep.memory.full) {
 			hauler.recharge(creep);
 		} else {
