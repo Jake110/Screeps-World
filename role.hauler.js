d5c19b1e020c547de02bd6f891f2d7004e4d5eba
Jake Joules <jake2105@gmail.com>
2026-02-25 12:55:51 +0000

    spelling

--- role.hauler.js
+++ role.hauler.js
@@ -1,12 +1,12 @@
 const hauler = require("creep.hauler");
 
-module.export = {
-    run: function (creep) {
-        hauler.check_capacity(creep, RESOURCE_ENERGY)
-        if (creep.memory.full) {
-            hauler.recharge(creep)
-        } else {
-            hauler.collect(creep)
-        }
-    }
-}
+module.exports = {
+	run: function (creep) {
+		hauler.check_capacity(creep, RESOURCE_ENERGY);
+		if (creep.memory.full) {
+			hauler.recharge(creep);
+		} else {
+			hauler.collect(creep);
+		}
+	},
+};
