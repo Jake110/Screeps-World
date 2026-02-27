3bafe34026fb98ba353a22d64cc317ac111b787f
Jake Joules <jake2105@gmail.com>
2026-02-27 15:32:46 +0000

    refill creeps that have space instead of who are looking to refill

--- creep.hauler.js
+++ creep.hauler.js
@@ -20,7 +20,7 @@ function get_collection_target(creep, find_list, storage_override = false) {
 								_creep_memory.role == "harvester") ||
 								(creep_memory.role == "worker" &&
 									_creep_memory.role == "hauler")) &&
-							_creep_memory.full
+							_creep.store.getFreeCapacity() > 0
 						);
 					}
 					if (options.deathTime) {
