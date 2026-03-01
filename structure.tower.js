bbea236c183c553a0e650e3378443c77f2fb9e9b
Jake Joules <Jake2105@gmail.com>
2026-03-01 12:35:50 +0000

    overhaul tower control, should reduce CPU usage for multiple towers

--- structure.tower.js
+++ structure.tower.js
@@ -2,56 +2,101 @@ const combat = require("utility.combat");
 
 module.exports = {
 	fire: function (room) {
+		let towers = [];
 		room.find(FIND_MY_STRUCTURES, {
 			filter: { structureType: STRUCTURE_TOWER },
 		}).forEach(function (tower) {
-			let energy = tower.store[RESOURCE_ENERGY];
-			let capacity = tower.store.getCapacity();
 			let acted = false;
-			[(10, 20, 50)].forEach(function (range) {
-				if (!acted) {
-					let hostile = combat.ranged_target(tower.pos, range);
-					if (hostile) {
-						tower.attack(hostile);
-						acted = true;
+			let fire = function (range) {
+				let hostile = combat.ranged_target(tower.pos, range);
+				if (hostile) {
+					tower.attack(hostile);
+					acted = true;
+				}
+			};
+			let heal = function (range) {
+				let most_damaged_creep;
+				let hit_percentage = 1;
+				tower.pos
+					.findInRange(FIND_MY_CREEPS, range)
+					.forEach(function (creep) {
+						if (creep.hits / creep.hitsMax < hit_percentage) {
+							most_damaged_creep = creep;
+						}
+					});
+				if (most_damaged_creep) {
+					tower.heal(most_damaged_creep);
+					acted = true;
+				}
+			};
+			[(fire, heal)].forEach(function (action) {
+				[(10, 20, 50)].forEach(function (range) {
+					if (!acted) {
+						action(range);
 					}
-				}
+				});
 			});
-			if (!acted && energy > capacity / 4) {
-				let damaged_structure = tower.pos.findClosestByRange(
-					FIND_STRUCTURES,
-					{
-						filter: function (structure) {
-							if (structure.owner && !structure.my) {
-								// Ignore structures out of range and owned by another player
-								return false;
-							}
-							return (
-								structure.hits < structure.hitsMax &&
-								![STRUCTURE_RAMPART, STRUCTURE_WALL].includes(
-									structure.structureType,
-								)
-							);
-						},
-					},
-				);
-				if (damaged_structure) {
-					tower.repair(damaged_structure);
-					acted = true;
-				}
+			if (
+				!acted &&
+				tower.store[RESOURCE_ENERGY] > tower.store.getCapacity() / 4
+			) {
+				towers.push(tower);
 			}
-			if (!acted && energy > capacity / 3) {
-				let damaged_creep = tower.pos.findClosestByRange(
-					FIND_MY_CREEPS,
-					{
-						filter: function (creep) {
-							return creep.hits < creep.hitsMax;
-						},
-					},
-				);
-				if (damaged_creep) {
-					tower.heal(damaged_creep);
-					acted = true;
+		});
+		let emergency_repair = function (tower) {
+			return tower.pos.findClosestByRange(FIND_STRUCTURES, {
+				filter: function (structure) {
+					if (
+						(structure.owner && !strustructure.my) ||
+						![STRUCTURE_RAMPART, STRUCTURE_WALL].includes(
+							structure.structureType,
+						)
+					) {
+						// Ignore ramparts that aren't mine and everything else that isn't a wall
+						return false;
+					}
+					return structure.hits < structure.hitsMax / 4;
+				},
+			});
+		};
+		let repair = function (tower) {
+			return tower.pos.findClosestByRange(FIND_STRUCTURES, {
+				filter: function (structure) {
+					if (
+						(structure.owner && !structure.my) ||
+						[STRUCTURE_RAMPART, STRUCTURE_WALL].includes(
+							structure.structureType,
+						)
+					) {
+						// Ignore structures owned by another player, ramparts, and walls
+						return false;
+					}
+					return structure.hits < structure.hitsMax;
+				},
+			});
+		};
+		let bolster_defence = function (tower) {
+			return tower.pos.findClosestByRange(FIND_STRUCTURES, {
+				filter: function (structure) {
+					if (
+						(structure.owner && !strustructure.my) ||
+						![STRUCTURE_RAMPART, STRUCTURE_WALL].includes(
+							structure.structureType,
+						)
+					) {
+						// Ignore ramparts that aren't mine and everything else that isn't a wall
+						return false;
+					}
+					return structure.hits < structure.hitsMax;
+				},
+			});
+		};
+		[emergency_repair, repair, bolster_defence].forEach(function (action) {
+			while (towers.length > 0) {
+				let tower = towers.pop();
+				let target = action(tower);
+				if (target) {
+					tower.repair(target);
 				}
 			}
 		});
