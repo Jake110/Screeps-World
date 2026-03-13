d733b828660e2545ad8bee69db2a33335ce11e2c
Jake Joules <Jake2105@gmail.com>
2026-03-13 07:26:20 +0000

    that's not a function

--- role.worker.js
+++ role.worker.js
@@ -6,7 +6,7 @@ module.exports = {
 	run: function (creep) {
 		hauler.capacity_check(creep, RESOURCE_ENERGY);
 		if (creep.memory.full) {
-			if (creep.room.controller.ticksToDowngrade() > 1000) {
+			if (creep.room.controller.ticksToDowngrade > 1000) {
 				if (hauler.recharge(creep)) {
 					return null;
 				}
