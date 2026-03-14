85333959fb593b801a287ff43975db68374a2819
Jake Joules <Jake2105@gmail.com>
2026-03-14 21:41:57 +0000

    need to add links to memory setup

--- utility.memory.js
+++ utility.memory.js
@@ -1,4 +1,11 @@
-let structure_names = ["containers", "extensions", "spawns", "towers", "walls"];
+let structure_names = [
+	"containers",
+	"extensions",
+	"links",
+	"spawns",
+	"towers",
+	"walls",
+];
 
 function coord_to_pos(coord, room) {
 	let split_coord = coord.split(":");
