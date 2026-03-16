d0b32f941d963e964bb63b5f207a2c7f97187e09
Jake Joules <jake2105@gmail.com>
2026-03-16 11:07:21 +0000

    deposit energy as you harvest when there are links and containers next to you

--- creep.harvest.js
+++ creep.harvest.js
@@ -6,7 +6,7 @@ const worker = require("creep.worker");
 module.exports = {
 	/** @param {Creep} creep **/
 	harvest: function (creep) {
-		let target = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE, {
+		let harvest_target = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE, {
 			filter: function (_source) {
 				let no_rivals = true;
 				if (creep.body.length > 4) {
@@ -23,50 +23,79 @@ module.exports = {
 				return combat.avoid_filter && no_rivals;
 			},
 		});
-		if (target) {
-			if (creep.harvest(target) == ERR_NOT_IN_RANGE) {
-				creep.moveTo(target, {
+		if (harvest_target) {
+			if (creep.harvest(harvest_target) == ERR_NOT_IN_RANGE) {
+				creep.moveTo(harvest_target, {
 					visualizePathStyle: { stroke: "#fff23e" },
 				});
+			}
+		}
+		let deposit_target = creep.pos.findInRange(FIND_MY_STRUCTURES, 1, {
+			filter: { structureType: STRUCTURE_LINK },
+		});
+		if (deposit_target.length > 0) {
+			deposit_target = deposit_target[0];
+			creep.transfer(deposit_target, RESOURCE_ENERGY);
+			if (deposit_target.store.getFreeCapacity(RESOURCE_ENERGY) == 0) {
+				let core_link = quartermaster.get_structure(
+					creep.room,
+					creep.room.memory.links[0],
+					STRUCTURE_LINK,
+				);
+				deposit_target.transferEnergy(core_link);
+			}
+		} else {
+			deposit_target = creep.pos.findInRange(FIND_STRUCTURES, 1, {
+				filter: { structureType: STRUCTURE_CONTAINER },
+			});
+			if (deposit_target.length > 0) {
+				deposit_target = deposit_target[0];
+				creep.transfer(deposit_target, RESOURCE_ENERGY);
 			}
 		}
 	},
 
 	/** @param {Creep} creep **/
 	deposit: function (creep) {
-		let target = creep.pos.findInRange(FIND_MY_STRUCTURES, 4, {
+		let deposit_target = creep.pos.findInRange(FIND_MY_STRUCTURES, 4, {
 			filter: { structureType: STRUCTURE_LINK },
 		});
-		if (target.length > 0) {
-			target = target[0];
-			if (creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-				creep.moveTo(target, {
+		if (deposit_target.length > 0) {
+			deposit_target = deposit_target[0];
+			if (
+				creep.transfer(deposit_target, RESOURCE_ENERGY) ==
+				ERR_NOT_IN_RANGE
+			) {
+				creep.moveTo(deposit_target, {
 					visualizePathStyle: { stroke: "#2bff00" },
 				});
 			}
-			if (target.store.getFreeCapacity(RESOURCE_ENERGY) == 0) {
+			if (deposit_target.store.getFreeCapacity(RESOURCE_ENERGY) == 0) {
 				let core_link = quartermaster.get_structure(
 					creep.room,
 					creep.room.memory.links[0],
 					STRUCTURE_LINK,
 				);
-				target.transferEnergy(core_link);
+				deposit_target.transferEnergy(core_link);
 			}
 		}
-		if (target.length == 0) {
-			target = creep.pos.findInRange(FIND_STRUCTURES, 1, {
+		if (deposit_target.length == 0) {
+			deposit_target = creep.pos.findInRange(FIND_STRUCTURES, 1, {
 				filter: { structureType: STRUCTURE_CONTAINER },
 			});
 		}
-		if (target.length == 0) {
+		if (deposit_target.length == 0) {
 			if (hauler.recharge(creep)) {
 				return null;
 			}
 			worker.upgrade(creep);
 		} else {
-			target = target[0];
-			if (creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-				creep.moveTo(target, {
+			deposit_target = deposit_target[0];
+			if (
+				creep.transfer(deposit_target, RESOURCE_ENERGY) ==
+				ERR_NOT_IN_RANGE
+			) {
+				creep.moveTo(deposit_target, {
 					visualizePathStyle: { stroke: "#2bff00" },
 				});
 			}
