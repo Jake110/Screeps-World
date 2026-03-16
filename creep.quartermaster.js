3a26f4f3fe1de97ffd64ec47b04dda7101548e81
Jake Joules <jake2105@gmail.com>
2026-03-16 09:49:43 +0000

    simplify logic

--- creep.quartermaster.js
+++ creep.quartermaster.js
@@ -2,21 +2,22 @@ const memory = require("utility.memory");
 
 module.exports = {
 	get_structure: function (room, coord, structure_type) {
-		let structure = null;
+		let found_structure = null;
 		memory
 			.coord_to_pos(coord, room)
 			.look()
 			.forEach(function (item) {
-				if (item.structure) {
-					if (item.structure.structureType == structure_type) {
-						structure = item;
+				let structure = item.structure;
+				if (structure) {
+					if (structure.structureType == structure_type) {
+						found_structure = structure;
 						console.log(
 							"Found [" + structure_type + "] at [" + coord + "]",
 						);
 					}
 				}
 			});
-		console.log("\tReturning: " + structure);
-		return structure;
+		console.log("\tReturning: " + found_structure);
+		return found_structure;
 	},
 };
