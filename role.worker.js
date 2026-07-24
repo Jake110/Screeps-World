a69ee66fd106f56bcf37525bab5e196efb610287
Jake Joules <jake2105@gmail.com>
2026-07-24 10:13:34 +0100

    only build before recharging when there is no spawn

--- role.worker.js
+++ role.worker.js
@@ -7,10 +7,18 @@ module.exports = {
 		hauler.capacity_check(creep, RESOURCE_ENERGY);
 		if (creep.memory.full) {
 			if (creep.room.controller.ticksToDowngrade > 1000) {
-				if (worker.build(creep)) {
-					return null;
+				if (
+					creep.room.memory.core &&
+					creep.room.find(FIND_MY_SPAWNS).length == 0
+				) {
+					if (worker.build(creep)) {
+						return null;
+					}
 				}
 				if (hauler.recharge(creep)) {
+					return null;
+				}
+				if (worker.build(creep)) {
 					return null;
 				}
 			}
