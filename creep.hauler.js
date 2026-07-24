a994e71acd465f63ab85c04ce4408b2c2f755356
Jake Joules <jake2105@gmail.com>
2026-07-24 10:10:58 +0100

    remove logging

--- creep.hauler.js
+++ creep.hauler.js
@@ -34,14 +34,6 @@ function get_collection_target(
 						return false;
 					}
 					if (extension_override) {
-						if (option.structureType == STRUCTURE_EXTENSION) {
-							console.log(
-								"Checking [" +
-									option.pos +
-									"]. Energy: " +
-									option.store[RESOURCE_ENERGY],
-							);
-						}
 						return (
 							option.structureType == STRUCTURE_EXTENSION &&
 							option.store[RESOURCE_ENERGY] > 0
